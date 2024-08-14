fd7778311135cf112e88f16e456f7d859045da3d
Hilal <hilaltaha@hotmail.com>
2024-08-14 18:45:58 +0200

    The website and excutable file

---
+++ EAGL_excutor.py
@@ -0,0 +1,26 @@
+import os
+import random
+
+from bs4 import BeautifulSoup
+import Classes.PredictionClass as PC
+
+
+def get_random_element(website: str):
+    soup = BeautifulSoup(website, 'html.parser')
+    extract_elements = soup.find_all()
+    exclude_tags = ["a", "body", "html", "link", "meta", "script", "head"]
+    filtered_elements = [element for element in extract_elements if element.name not in exclude_tags]
+    element = random.sample(filtered_elements, 1)
+    return element
+
+
+if __name__ == "__main__":
+    path_to_model = os.getcwd() + "\\cross_training.joblib"
+    path_to_html = "Medium.html"
+
+    with open(path_to_html, 'r', encoding='utf-8') as file:
+        website = file.read()
+
+    target_element = get_random_element(website)
+    locators, time_to_generate = PC.generate_locators_prediction_model(target_element, path_to_model)
+    print(locators, time_to_generate)
